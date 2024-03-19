5d393ceca86f406d02afba96164ad3f6893d8dcf
suisuiz <wangyumin1996@gmail.com>
2024-03-19 19:39:03 +0800

    feat: add APP package

---
+++ App.vue
@@ -0,0 +1,43 @@
+<script>
+	export default {
+		onLaunch: function() {
+			console.log('App Launch')
+		},
+		onShow: function() {
+			console.log('App Show')
+			
+			uni.getSystemInfo({
+				success(res) {
+					console.log(res);
+				}
+			})
+		},
+		onHide: function() {
+			console.log('App Hide')
+		}
+	}
+</script>
+
+<style>
+/* APP-NVUE 不编译，其他编译 */
+/* #ifndef APP-NVUE */
+@import '@/static/css/uni.css';
+/* #endif */
+
+@import '@/static/css/uni-nvue.css';
+
+/* 仅 MP-TOUTIAO 编译 */
+/* #ifdef MP-TOUTIAO */
+@font-face {
+  font-family: uniicons;
+  src: url('/static/uni.ttf');
+}
+
+/* #endif */
+
+/* APP-NVUE 不编译，其他编译 */
+/* #ifndef APP-NVUE */
+@import '@/static/css/index.css';
+/* #endif */
+
+</style>
