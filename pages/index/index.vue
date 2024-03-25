395857e5e1676ac8ce7fb1c9306ad2646aed0f32
suisuiz <wangyumin1996@gmail.com>
2024-03-25 16:23:38 +0800

    feat: page_index

---
+++ pages/index/index.vue
@@ -0,0 +1,68 @@
+<template>
+  <view class="content">
+    <view class="item" v-for="(item, index) in arr" :key="index" @click="tap(item.url)">{{ item.text }} </view>
+  </view>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      arr: [
+        {
+          text: '测试一',
+          url: 'form'
+        },
+        {
+          text: '截图',
+          url: 'screenshot'
+        },
+        {
+          text: '拍照',
+          url: 'camera'
+        },
+				{
+				  text: '日历 vue',
+				  url: 'calendar'
+				},
+				{
+				  text: '时间线',
+				  url: 'daylist'
+				},
+        // {
+        //   text: 'uCharts 图表',
+        //   url: 'uCharts'
+        // },
+        // {
+        //   text: 'ECharts 图表 + renderjs',
+        //   url: 'ECharts'
+        // },
+      ]
+    }
+  },
+  onShow() {
+    plus.screen.lockOrientation('portrait')
+  },
+  methods: {
+    tap(url) {
+      uni.navigateTo({
+        url: `/pages/content/${url}`
+      })
+    }
+  }
+}
+</script>
+
+<style>
+.content {
+  padding: 0 30px;
+  box-sizing: border-box;
+}
+
+.item {
+  padding: 10px;
+  box-sizing: border-box;
+  margin-top: 20px;
+  background: skyblue;
+}
+</style>
